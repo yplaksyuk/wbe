2ef66391c14aa4227cf9373859ea6645e1574767
Yuri Plaksyuk <yuri.plaksyuk@chestnutcorp.com>
2021-01-09 21:11:02 +0200

    Added scale and thinkness. General UI improvements.

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 $(function() {
+	const init = {
+		scale: 0.5,
+		thinkness: 0.2,
+		source: '111\n222\n333'
+	}
+
+	const dest = $('#dest').empty();
 
 	const update = function() {
-		const dest = $('#dest').empty();
 		const source = $('#source').val();
 
 		$.each(source.replace(/[^0-9 \n]/, '').split('\n'), function(index, text) {
@@ -23,7 +29,7 @@ $(function() {
 						elem.setAttributeNS(null, 'stroke-width', '.5');
 					}
 					else {
-						elem.setAttributeNS(null, 'stroke-dasharray', '.5 1');
+						elem.setAttributeNS(null, 'stroke-dasharray', '1 1.5');
 					}
 
 					dest.append(elem);
@@ -35,11 +41,32 @@ $(function() {
 
 	$('#panel').tabs();
 
+	const scale = $('#scale').slider({ min: 0.3, max: 1.0, step: 0.1 })
+		.on('slidechange slide', function() {
+			const val = scale.slider('value');
+
+			dest.attr('transform', `scale(${val} ${val})`);
+			$('#bge').attr('patternTransform', `scale(${val} ${val})`);
+		});
+
+	const thinkness = $('#thinkness').slider({ min: 0.1, max: 0.7, step: 0.1 })
+		.on('slidechange slide', function() {
+			const val = thinkness.slider('value');
+
+			dest.attr('stroke-width', val);
+		});
+
 	$('#source').on('keydown change', function() {
 		setTimeout(update, 0);
 	});
 
-	$('#source')
-		.val('1111111111\n2222222222\n3333333333\n4444444444\n5555555555\n6666666666\n7777777777\n8888888888\n9999999999\n0000000000')
-		.trigger('change');
+	$('.inject').each(function() {
+		const elem = $(this);
+		const value = init[elem.attr('id')];
+
+		if (elem.is('.ui-slider'))
+			elem.slider('value', value);
+		else
+			elem.val(value).trigger('change');
+	});
 });
