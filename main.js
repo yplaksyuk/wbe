b03697be8ed9e5d43f0b4fa927ab4ea3a96b36df
Yuri Plaksyuk <yuri.plaksyuk@chestnutcorp.com>
2021-01-10 16:14:58 +0200

    More styles and settings persistence.

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 $(function() {
+	const storage = window.localStorage;
+
 	const init = {
 		scale: 0.5,
 		thinkness: 0.2,
+		color: 1,
+		style: false,
 		source: '111\n222\n333'
 	}
 
-	const dest = $('#dest').empty();
+	const dest = $('#dest');
 
 	const update = function() {
 		const source = $('#source').val();
+
+		dest.empty();
 
 		$.each(source.replace(/[^0-9 \n]/, '').split('\n'), function(index, text) {
 			const y = index * 60;
@@ -27,33 +33,58 @@ $(function() {
 					if (pos == 0) {
 						elem.setAttributeNS(null, 'stroke', 'red');
 						elem.setAttributeNS(null, 'stroke-width', '.5');
-					}
-					else {
-						elem.setAttributeNS(null, 'stroke-dasharray', '1 1.5');
+						elem.setAttributeNS(null, 'stroke-dasharray', 'none');
 					}
 
 					dest.append(elem);
 				}
 			}
 		});
+
+		storage.setItem('source', source);
 	};
 
 
 	$('#panel').tabs();
 
-	const scale = $('#scale').slider({ min: 0.3, max: 1.0, step: 0.1 })
+	const scale = $('#scale').slider({ min: 0.3, max: 1.0, step: 0.01 })
 		.on('slidechange slide', function(event, ui) {
 			const val = ui.value || scale.slider('value');
 
 			dest.attr('transform', `scale(${val} ${val})`);
 			$('#bge').attr('patternTransform', `scale(${val} ${val})`);
+
+			storage.setItem('scale', val);
 		});
 
-	const thinkness = $('#thinkness').slider({ min: 0.1, max: 0.7, step: 0.1 })
+	const thinkness = $('#thinkness').slider({ min: 0.1, max: 0.7, step: 0.01 })
 		.on('slidechange slide', function(event, ui) {
 			const val = ui.value || thinkness.slider('value');
 
 			dest.attr('stroke-width', val);
+
+			storage.setItem('thinkness', val);
+		});
+
+	const color = $('#color').slider({ min: 0, max: 1, step: 0.01 })
+		.on('slidechange slide', function(event, ui) {
+			const val = ui.value || color.slider('value');
+
+			dest.attr('stroke', `rgba(0,0,0,${val})`);
+
+			storage.setItem('color', val);
+		});
+
+	const style = $('#style')
+		.on('click change', function() {
+			const val = style.prop('checked');
+
+			if (val)
+				dest.each(function() { this.setAttributeNS(null, 'stroke-dasharray', '1 1.5'); });
+			else
+				dest.each(function() { this.removeAttributeNS(null, 'stroke-dasharray'); });
+
+			storage.setItem('style', val);
 		});
 
 	$('#source').on('keydown change', function() {
@@ -62,11 +93,16 @@ $(function() {
 
 	$('.inject').each(function() {
 		const elem = $(this);
-		const value = init[elem.attr('id')];
+		const name = elem.attr('id');
+		const value = storage.getItem(name) || init[name];
 
 		if (elem.is('.ui-slider'))
 			elem.slider('value', value);
+		else if (elem.is(':checkbox'))
+			elem.prop('checked', value === 'true');
 		else
-			elem.val(value).trigger('change');
+			elem.val(value);
+
+		elem.trigger('change');
 	});
 });
